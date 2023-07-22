524a1b3c8f99e5fcbe5184e448b5c7daf070734a
Yufei <dianazyf@gmail.com>
2023-07-22 18:06:07 -0400

    Add more personal stats

--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -18,6 +18,24 @@ const ResearchAge = () => {
     return <>{researchAge}</>;
 };
 
+const Cups = () => {
+    const [cups, setCups] = useState();
+
+    const tick = () => {
+        const divisor = 1000 * 60 * 60 * 24; // ms in aday
+        const espressoMachinePurchasedDate = new Date('2021-08-19T07:30:00');
+        setCups(((Date.now() - espressoMachinePurchasedDate) / divisor *5 / 7).toFixed(5));
+    };
+
+    useEffect(() => {
+        const timer = setInterval(() => tick(), 25);
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+    return <>{cups}</>;
+};
+
 const data = [
     {
         key: 'age',
@@ -25,12 +43,32 @@ const data = [
         value: <ResearchAge />,
     },
     {
+        key: 'coffee',
+        label: 'Cups of Espresso Consumed (Double Shot)',
+        value: <Cups />,
+    },
+    {
         key: 'countries',
-        label: 'Countries & Regions visited',
+        label: 'Countries & Regions Visited',
         value: 16,
         link:
             'https://www.google.com/maps/d/u/0/edit?mid=1FUcVhy3_wVpZFL-px86SnecoHtTncAo&usp=sharing',
-    }
+    },
+    {
+        key: 'pets',
+        label: 'Furry Friends at Home',
+        value: 3,
+    },
+    {
+        key: 'tricks',
+        label: 'Hours Spent Teaching Tricks',
+        value: 'Hundreds',
+    },
+    {
+        key: 'pets',
+        label: 'Number of Tricks My Doggies Mastered',
+        value: 'None',
+    },
 ];
 
 export default data;
